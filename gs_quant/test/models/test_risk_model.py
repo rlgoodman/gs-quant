rlgoodman/gs-quant

commit 954274e91b063a4819d13d34a18c3006aedac1c3

Update to dataclasses (#221)

gs_quant/models/risk_model_utils.py
@@ -1,0 +1,252 @@
+"""
+Copyright 2021 Goldman Sachs.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+"""
+from typing import List
+import pandas as pd
+import datetime as dt
+import math
+
+from gs_quant.target.risk_models import RiskModelData
+
+from gs_quant.api.gs.risk_models import GsFactorRiskModelApi
+
+
+def build_asset_data_map(results: List, universe: List, measure: str) -> dict:
+    if not results:
+        return {}
+    data_map = {}
+    for asset in universe:
+        date_list = {}
+        for row in results:
+            if asset in row.get('assetData').get('universe'):
+                i = row.get('assetData').get('universe').index(asset)
+                date_list[row.get('date')] = row.get('assetData').get(measure)[i]
+        data_map[asset] = date_list
+    return data_map
+
+
+def build_factor_data_map(results: List, identifier: str) -> dict:
+    factor_data = [factor.get('factorId') for factor in results[0].get('factorData')]
+    data_map = {}
+    for factor in factor_data:
+        date_list = {}
+        factor_name = factor
+        for row in results:
+            for data in row.get('factorData'):
+                if data.get('factorId') == factor:
+                    factor_name = factor if identifier == 'id' else data.get(identifier)
+                    date_list[row.get('date')] = data.get('factorReturn')
+        data_map[factor_name] = date_list
+    return data_map
+
+
+def build_factor_data_dataframe(results: List, identifier: str) -> pd.DataFrame:
+    data_map = {}
+    date_list = []
+    for row in results:
+        date_list.append(row.get('date'))
+        for data in row.get('factorData'):
+            factor_name = data.get('factorId') if identifier == 'id' else data.get(identifier)
+            factor_return = data.get('factorReturn')
+            if factor_name in data_map.keys():
+                factor_returns = data_map.get(factor_name)
+                factor_returns.append(factor_return)
+            else:
+                factor_returns = [factor_return]
+            data_map[factor_name] = factor_returns
+        data_map['date'] = date_list
+    data_frame = pd.DataFrame(data_map).set_index('date')
+    return data_frame
+
+
+def build_pfp_data_dataframe(results: List) -> pd.DataFrame:
+    date_list = []
+    pfp_list = []
+    for row in results:
+        pfp_map = dict()
+        pfp_map['assetId'] = row.get('factorPortfolios').get('universe')
+        for factor in row.get('factorPortfolios').get('portfolio'):
+            factor_id = factor.get('factorId')
+            pfp_map[f'factorId: {factor_id}'] = factor.get('weights')
+        weights_df = pd.DataFrame(pfp_map)
+        pfp_list.append(weights_df)
+        date_list.append(row.get('date'))
+    data = pd.concat(pfp_list, keys=date_list)
+    return data
+
+
+def get_isc_dataframe(results: dict) -> pd.DataFrame:
+    cov_list = []
+    date_list = []
+    for row in results:
+        matrix_df = pd.DataFrame(row.get('issuerSpecificCovariance'))
+        cov_list.append(matrix_df)
+        date_list.append(row.get('date'))
+    data = pd.concat(cov_list, keys=date_list)
+    return data
+
+
+def get_covariance_matrix_dataframe(results: dict) -> pd.DataFrame:
+    cov_list = []
+    date_list = []
+    for row in results:
+        matrix_df = pd.DataFrame(row.get('covarianceMatrix'))
+        factor_names = [data.get('factorName') for data in row.get('factorData')]
+        matrix_df.columns = factor_names
+        matrix_df.index = factor_names
+        cov_list.append(matrix_df)
+        date_list.append(row.get('date'))
+    data = pd.concat(cov_list, keys=date_list)
+    return data
+
+
+def get_closest_date_index(date: dt.date, dates: List[str], direction: str) -> int:
+    for i in range(50):
+        for index in range(len(dates)):
+            if direction == 'before':
+                next_date = (date - dt.timedelta(days=i)).strftime('%Y-%m-%d')
+            else:
+                next_date = (date + dt.timedelta(days=i)).strftime('%Y-%m-%d')
+            if next_date == dates[index]:
+                return index
+    return -1
+
+
+def divide_request(data, n):
+    for i in range(0, len(data), n):
+        yield data[i:i + n]
+
+
+def batch_and_upload_partial_data(model_id: str, data: dict, max_asset_size):
+    """ Takes in total risk model data for one day and batches requests according to
+    asset data size, returns a list of messages from resulting post calls"""
+    date = data.get('date')
+    if data.get('factorData'):
+        factor_data = {
+            'date': date,
+            'factorData': data.get('factorData')}
+        if data.get('covarianceMatrix'):
+            factor_data['covarianceMatrix'] = data.get('covarianceMatrix')
+        print('Uploading factor data')
+        print(GsFactorRiskModelApi.upload_risk_model_data(
+            model_id,
+            factor_data,
+            partial_upload=True)
+        )
+
+    if data.get('assetData'):
+        asset_data_list, target_size = _batch_input_data({'assetData': data.get('assetData')}, max_asset_size)
+        for asset_data_batch in asset_data_list:
+            print(GsFactorRiskModelApi.upload_risk_model_data(
+                model_id,
+                {'assetData': asset_data_batch, 'date': date},
+                partial_upload=True,
+                target_universe_size=target_size)
+            )
+
+    if 'issuerSpecificCovariance' in data.keys() or 'factorPortfolios' in data.keys():
+        for optional_input_key in ['issuerSpecificCovariance', 'factorPortfolios']:
+            if data.get(optional_input_key):
+                optional_data = data.get(optional_input_key)
+                optional_data_list, target_size = _batch_input_data({optional_input_key: optional_data}, max_asset_size)
+                print(f'{optional_input_key} being uploaded for {date}...')
+                for optional_data_batch in optional_data_list:
+                    print(GsFactorRiskModelApi.upload_risk_model_data(
+                        model_id,
+                        {optional_input_key: optional_data_batch, 'date': date},
+                        partial_upload=True,
+                        target_universe_size=target_size)
+                    )
+
+
+def risk_model_data_to_json(risk_model_data: RiskModelData) -> dict:
+    risk_model_data = risk_model_data.to_json()
+    risk_model_data['assetData'] = risk_model_data.get('assetData').to_json()
+    if risk_model_data.get('factorPortfolios'):
+        risk_model_data['factorPortfolios'] = risk_model_data.get('factorPortfolios').to_json()
+        risk_model_data['factorPortfolios']['portfolio'] = [portfolio.to_json() for portfolio in
+                                                            risk_model_data.get('factorPortfolios').get(
+                                                                'portfolio')]
+    if risk_model_data.get('issuerSpecificCovariance'):
+        risk_model_data['issuerSpecificCovariance'] = risk_model_data.get('issuerSpecificCovariance').to_json()
+    return risk_model_data
+
+
+def get_universe_size(data_to_split: dict) -> int:
+    # takes any chunk of risk model data and returns the universe size
+    if 'assetData' in data_to_split.keys():
+        return len(data_to_split.get('assetData').get('universe'))
+    data_to_split = list(data_to_split.values())
+    for data in data_to_split:
+        if 'universe' in data.keys():
+            return len(data.get('universe'))
+        if 'universeId1' in data.keys():
+            return len(set(data.get('universeId1') +
+                           data.get('universeId1')))
+    raise ValueError(f'No universe found for data {data_to_split}')
+
+
+def _batch_input_data(input_data: dict, max_asset_size: int):
+    data_key = list(input_data.keys())[0]
+    target_universe_size = get_universe_size(input_data)
+    split_num = math.ceil(target_universe_size / max_asset_size) if math.ceil(
+        target_universe_size / max_asset_size) else 1
+    split_idx = math.ceil(target_universe_size / split_num)
+    batched_data_list = []
+    for i in range(split_num):
+        if data_key == 'assetData':
+            data_batched = _batch_asset_input(input_data.get('assetData'), i, split_idx, split_num,
+                                              target_universe_size)
+        elif data_key == 'factorPortfolios':
+            data_batched = _batch_pfp_input(input_data.get('factorPortfolios'), i, split_idx,
+                                            split_num, target_universe_size)
+        else:
+            data_batched = _batch_isc_input(input_data.get('issuerSpecificCovariance'), i, split_idx, split_num,
+                                            target_universe_size)
+        batched_data_list.append(data_batched)
+    return batched_data_list, target_universe_size
+
+
+def _batch_asset_input(input_data: dict, i: int, split_idx: int, split_num: int, target_universe_size: int) -> dict:
+    end_idx = (i + 1) * split_idx if split_num != i + 1 else target_universe_size + 1
+    asset_data_subset = {'universe': input_data.get('universe')[i * split_idx:end_idx],
+                         'specificRisk': input_data.get('specificRisk')[i * split_idx:end_idx],
+                         'factorExposure': input_data.get('factorExposure')[i * split_idx:end_idx]}
+    optional_asset_inputs = ['totalRisk', 'historicalBeta', 'specificReturn']
+    for optional_input in optional_asset_inputs:
+        if input_data.get(optional_input):
+            asset_data_subset[optional_input] = input_data.get(optional_input)[i * split_idx:end_idx]
+    return asset_data_subset
+
+
+def _batch_pfp_input(input_data: dict, i: int, split_idx: int, split_num: int, target_universe_size: int) -> dict:
+    end_idx = (i + 1) * split_idx if split_num != i + 1 else target_universe_size + 1
+    pfp_data_subset = dict()
+    universe_slice = input_data.get('universe')[i * split_idx:end_idx]
+    pfp_data_subset['universe'] = universe_slice
+    portfolio_slice = list()
+    for portfolio in input_data.get('portfolio'):
+        factor_id = portfolio.get('factorId')
+        weights_slice = portfolio.get('weights')[i * split_idx:end_idx]
+        portfolio_slice.append({"factorId": factor_id, "weights": weights_slice})
+    pfp_data_subset['portfolio'] = portfolio_slice
+    return pfp_data_subset
+
+
+def _batch_isc_input(input_data: dict, i: int, split_idx: int, split_num: int, target_universe_size: int) -> dict:
+    end_idx = (i + 1) * split_idx if split_num != i + 1 else target_universe_size + 1
+    return {'universeId1': input_data.get('universeId1')[i * split_idx:end_idx],
+            'universeId2': input_data.get('universeId2')[i * split_idx:end_idx],
+            'covariance': input_data.get('covariance')[i * split_idx:end_idx]}

gs_quant/test/api/test_risk_models.py
@@ -18,7 +18,7 @@ import datetime as dt
 
 from gs_quant.api.gs.risk_models import GsRiskModelApi, GsFactorRiskModelApi
 from gs_quant.session import *
-from gs_quant.target.risk_models import RiskModel, RiskModelFactor, RiskModelCalendar
+from gs_quant.target.risk_models import RiskModel, Factor, RiskModelCalendar
 
 
 def test_get_risk_models(mocker):
@@ -31,6 +31,7 @@ def test_get_risk_models(mocker):
                 "term": "Medium",
                 "vendor": "Goldman Sachs",
                 "universeIdentifier": "gsid",
+                "type": "Factor",
                 "version": 4
             }),
             RiskModel.from_dict({
@@ -40,7 +41,8 @@ def test_get_risk_models(mocker):
                 "term": "Medium",
                 "vendor": "Goldman Sachs",
                 "universeIdentifier": "gsid",
-                "version": 2
+                "version": 2,
+                "type": "Thematic"
             })
         ],
         'totalResults': 2
@@ -48,9 +50,9 @@ def test_get_risk_models(mocker):
 
     expected_response = [
         RiskModel(coverage='Global', id='WW_TEST_MODEL', name='World Wide Medium Term Test Model', term='Medium',
-                  vendor='Goldman Sachs', universe_identifier='gsid', version=4),
+                  vendor='Goldman Sachs', universe_identifier='gsid', version=4, type='Factor'),
         RiskModel(coverage='Global', id='WW_TEST_MODEL_2', name='World Wide Medium Term Test Model 2', term='Medium',
-                  vendor='Goldman Sachs', universe_identifier='gsid', version=2)
+                  vendor='Goldman Sachs', universe_identifier='gsid', version=2, type='Thematic')
     ]
 
     # mock GsSession
@@ -78,11 +80,13 @@ def test_get_risk_model(mocker):
         "term": "Medium",
         "vendor": "Goldman Sachs",
         "universeIdentifier": "gsid",
-        "version": 4
+        "version": 4,
+        "type": "Factor"
     })
 
     expected_response = RiskModel(coverage='Global', id='WW_TEST_MODEL', name='World Wide Medium Term Test Model',
-                                  term='Medium', vendor='Goldman Sachs', version=4, universe_identifier='gsid')
+                                  term='Medium', vendor='Goldman Sachs', version=4, universe_identifier='gsid',
+                                  type='Factor')
 
     # mock GsSession
     mocker.patch.object(
@@ -108,12 +112,13 @@ def test_create_risk_model(mocker):
         "term": "Medium",
         "vendor": "Goldman Sachs",
         "universeIdentifier": "gsid",
-        "version": 4
+        "version": 4,
+        "type": "Macro"
     })
 
     expected_response = RiskModel(coverage='Global', id='WW_TEST_MODEL', name='World Wide Medium Term Test Model',
-                                  term='Medium',
-                                  vendor='Goldman Sachs', version=4, universe_identifier='gsid')
+                                  term='Medium', vendor='Goldman Sachs', version=4, universe_identifier='gsid',
+                                  type='Macro')
 
     # mock GsSession
     mocker.patch.object(
@@ -138,7 +143,7 @@ def test_update_risk_model(mocker):
         "name": "World Wide Medium Term Test Model",
         "term": "Medium",
         "vendor": "Goldman Sachs",
-        "universeIdentifier": "gsid",
+        "RiskModelUniverseIdentifier": "gsid",
         "version": 4
     })
 
@@ -225,11 +230,11 @@ def test_upload_risk_model_calendar(mocker):
 
 def test_get_risk_model_factors(mocker):
     factors = {'results': [
-        RiskModelFactor.from_dict({
+        Factor.from_dict({
             "type": "Factor",
             "identifier": "Factor1"
         }),
-        RiskModelFactor.from_dict({
+        Factor.from_dict({
             "type": "Category",
             "identifier": "Factor2"
         })
@@ -238,8 +243,8 @@ def test_get_risk_model_factors(mocker):
     }
 
     expected_response = [
-        RiskModelFactor(identifier='Factor1', type='Factor'),
-        RiskModelFactor(identifier='Factor2', type='Category')
+        Factor(identifier='Factor1', type='Factor'),
+        Factor(identifier='Factor2', type='Category')
     ]
 
     # mock GsSession
@@ -254,17 +259,17 @@ def test_get_risk_model_factors(mocker):
 
     # run test
     response = GsFactorRiskModelApi.get_risk_model_factors(model_id='id')
-    GsSession.current._get.assert_called_with('/risk/models/id/factors', cls=RiskModelFactor)
+    GsSession.current._get.assert_called_with('/risk/models/id/factors', cls=Factor)
     assert response == expected_response
 
 
 def test_create_risk_model_factor(mocker):
-    factor = RiskModelFactor.from_dict({
+    factor = Factor.from_dict({
         "identifier": "Factor1",
         "type": "Factor"
     })
 
-    expected_response = RiskModelFactor(identifier='Factor1', type='Factor')
+    expected_response = Factor(identifier='Factor1', type='Factor')
 
     # mock GsSession
     mocker.patch.object(
@@ -278,12 +283,12 @@ def test_create_risk_model_factor(mocker):
 
     # run test
     response = GsFactorRiskModelApi.create_risk_model_factor(model_id='id', factor=factor)
-    GsSession.current._post.assert_called_with('/risk/models/id/factors', factor, cls=RiskModelFactor)
+    GsSession.current._post.assert_called_with('/risk/models/id/factors', factor, cls=Factor)
     assert response == expected_response
 
 
 def test_update_risk_model_factor(mocker):
-    factor = RiskModelFactor.from_dict({
+    factor = Factor.from_dict({
         "identifier": "factor",
         "type": "Factor"
     })
@@ -301,18 +306,18 @@ def test_update_risk_model_factor(mocker):
     # run test
     response = GsFactorRiskModelApi.update_risk_model_factor(model_id='id', factor=factor)
     GsSession.current._put.assert_called_with('/risk/models/{id}/factors/{identifier}'
-                                              .format(id='id', identifier='factor'), factor, cls=RiskModelFactor)
+                                              .format(id='id', identifier='factor'), factor, cls=Factor)
     assert response == factor
 
 
 def test_get_risk_model_coverage(mocker):
     results = {
         "results": [
-            RiskModelFactor.from_dict({
+            Factor.from_dict({
                 "model": "AXUS4S",
                 "businessDate": "2020-11-02"
             }),
-            RiskModelFactor.from_dict({
+            Factor.from_dict({
                 "model": "AXAU4M",
                 "businessDate": "2020-11-03"
             })

commit 68ebe986e2eb9358eeb1b7e6d06df9b8663ec4db

Fix problem introduced in 954274e9 when business_dates were changes to date objects instead of strings. covered by: SimCorp-Roy Goodman.dco

gs_quant/models/risk_model.py
@@ -14,6 +14,8 @@ specific language governing permissions and limitations
 under the License.
 """
 
+# Portions copyright SimCorp. Licensed under Apache 2.0 license
+
 import datetime as dt
 import math
 from enum import Enum, auto
@@ -366,17 +368,14 @@ class MarqueeRiskModel(RiskModel):
             start_date = posted_dates[0]
         if not end_date:
             end_date = dt.date.today() - dt.timedelta(days=1)
-        calendar = [
-            dt.datetime.strptime(date, "%Y-%m-%d").date()
-            for date in self.get_calendar(start_date=start_date, end_date=end_date).business_dates
-        ]
+        calendar = self.get_calendar(start_date=start_date, end_date=end_date).business_dates
         return [date for date in calendar if date not in posted_dates]
 
     def get_most_recent_date_from_calendar(self) -> dt.date:
         """Get T-1 date according to risk model calendar"""
         yesterday = dt.date.today() - dt.timedelta(1)
         calendar = self.get_calendar(end_date=yesterday).business_dates
-        return dt.datetime.strptime(calendar[len(calendar) - 1], '%Y-%m-%d').date()
+        return calendar[len(calendar) - 1]
 
     def save(self):
         """Upload current Risk Model object to Marquee"""

gs_quant/models/risk_model_utils.py
@@ -14,6 +14,8 @@ specific language governing permissions and limitations
 under the License.
 """
 
+# Portions copyright SimCorp. Licensed under Apache 2.0 license
+
 import datetime as dt
 import logging
 import math
@@ -232,13 +234,13 @@ def build_factor_volatility_dataframe(results: List, group_by_name: bool, factor
     return df
 
 
-def get_closest_date_index(date: dt.date, dates: List[str], direction: str) -> int:
+def get_closest_date_index(date: dt.date, dates: Union[tuple[dt.date, ...]|List[dt.date]], direction: str) -> int:
     for i in range(50):
         for index in range(len(dates)):
             if direction == 'before':
-                next_date = (date - dt.timedelta(days=i)).strftime('%Y-%m-%d')
+                next_date = date - dt.timedelta(days=i)
             else:
-                next_date = (date + dt.timedelta(days=i)).strftime('%Y-%m-%d')
+                next_date = date + dt.timedelta(days=i)
             if next_date == dates[index]:
                 return index
     return -1

gs_quant/test/api/test_risk_models.py
@@ -14,6 +14,8 @@ specific language governing permissions and limitations
 under the License.
 """
 
+# Portions copyright SimCorp. Licensed under Apache 2.0 license
+
 import datetime as dt
 
 from gs_quant.api.gs.risk_models import GsRiskModelApi, GsFactorRiskModelApi
@@ -222,6 +224,20 @@ def test_get_risk_model_calendar(mocker):
     response = GsRiskModelApi.get_risk_model_calendar('id')
     GsSession.current.sync.get.assert_called_with('/risk/models/{id}/calendar'.format(id='id'), cls=RiskModelCalendar)
     assert response == calendar
+
+
+def test_get_risk_model_dates(mocker):
+    # test fetching dates for model
+    calendar = RiskModelCalendar.from_dict({"businessDates": ["2020-03-31", "2020-04-30"]})
+    mocker.patch.object(
+        GsSession.__class__, 'default_value',
+        return_value=GsSession.get(Environment.QA, 'client_id', 'secret')
+    )
+    mocker.patch.object(GsSession.current.sync, 'get',
+                        return_value={'results' : calendar.businessDates})
+    response = GsRiskModelApi.get_risk_model_dates('id')
+    GsSession.current.sync.get.assert_called_with('/risk/models/{id}/dates?'.format(id='id'))
+    assert response == calendar.businessDates
 
 
 def test_upload_risk_model_calendar(mocker):

gs_quant/test/models/test_risk_model.py
@@ -14,6 +14,8 @@ specific language governing permissions and limitations
 under the License.
 """
 
+# Portions copyright SimCorp. Licensed under Apache 2.0 license
+
 import datetime as dt
 from unittest import mock
 
@@ -32,7 +34,7 @@ from gs_quant.target.risk_models import (
     RiskModelType,
     RiskModelDataAssetsRequest as DataAssetsRequest,
     RiskModelDataMeasure as Measure,
-    RiskModelUniverseIdentifierRequest as UniverseIdentifier,
+    RiskModelUniverseIdentifierRequest as UniverseIdentifier, RiskModelCalendar
 )
 
 empty_entitlements = {"execute": [], "edit": [], "view": [], "admin": [], "query": [], "upload": []}
@@ -1752,6 +1754,53 @@ def test_get_currency_exchange_rate(mocker):
     )
 
     assert_frame_equal(expected_data_frame, actual_data_frame, check_like=True)
+
+
+def _build_calendar() -> RiskModelCalendar:
+    # return a calendar covering March and April 2010 with only weekdays as business dates
+    business_dates = list()
+    start_date = dt.date(2010, 2, 28)
+    for i in range(63):
+        next_date = start_date + dt.timedelta(days=i)
+        if next_date.weekday() >= 5: # skip weekends
+            continue
+        business_dates.append(next_date)
+    return RiskModelCalendar.from_dict({"businessDates": [str(d) for d in business_dates]})
+
+
+def test_get_calendar(mocker):
+    model = mock_risk_model(mocker)
+    calendar = _build_calendar()
+    mocker.patch.object(GsSession.current.sync, 'get', return_value=calendar)
+
+    calendar_dates = model.get_calendar(start_date=dt.date(2010, 3, 7),
+                                        end_date=dt.date(2010, 4, 29))
+    assert len(calendar_dates.business_dates) == 39
+    assert calendar_dates.business_dates[0] == dt.date(2010, 3, 8)
+    assert calendar_dates.business_dates[-1] == dt.date(2010, 4, 29)
+
+
+def test_get_missing_dates(mocker):
+    def _return_calendar_or_dates(url: str, **kwargs):
+        if 'dates' in url:
+            return {'results': tuple(str(d) for d in dates)}
+        elif 'calendar' in url:
+            return RiskModelCalendar(business_dates=calendar.business_dates)
+        return None
+
+    model = mock_risk_model(mocker)
+    calendar = _build_calendar()
+    dates = list(calendar.business_dates)
+    dates.remove(dt.date(2010, 3, 9))
+    dates.remove(dt.date(2010, 4, 14))
+    mocker.patch.object(GsSession.current.sync, 'get',
+                        side_effect=_return_calendar_or_dates)
+
+    missing_dates = model.get_missing_dates(start_date=dt.date(2010, 3, 1),
+                                            end_date=dt.date(2010, 4, 29))
+    assert len(missing_dates) == 2
+    assert missing_dates[0] == dt.date(2010, 3, 9)
+    assert missing_dates[1] == dt.date(2010, 4, 14)
 
 
 if __name__ == "__main__":